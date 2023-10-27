0059986f9feffe91c6769b77693c988c736d022c
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2023-10-27 16:58:44 +0300

    friendsListItem and json

---
+++ src/components/Friends/FriendListItem/FriendListItem.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+export const FriendListItem = ({id, isOnline, avatar, name})=>{
+        return  (
+        <li key= {id} className="item">
+          <span className="status">{isOnline}</span>
+          <img className="avatar" src={avatar} alt={name} width="48" />
+          <p className="name">{name}</p>
+        </li>
+)      
+}
+
+
+
+FriendListItem.protoType = {
+    id: PropTypes.number.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+}
+
